3d34ef617e02bcb4f1dc86352a9431297b2fb1fc
VPA Parts <vpa-parts@github.com>
2026-01-30 22:17:26 +0000

    Bump SW cache to v2 to force update

--- sw.js
+++ sw.js
@@ -1,20 +1,33 @@
-const CACHE_NAME = 'uc-cache-v1';
+const CACHE_NAME = 'uc-cache-v2';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/icon-512.png'
+    './',
+    './index.html',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force new SW to activate immediately
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Take control of all clients immediately
+    );
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
